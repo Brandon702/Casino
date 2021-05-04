b6dfbcd1ddcbb247a3c2070b17657ef17e311959
Brandon Goldsworthy <BGoldsworthy@neumont.edu>
2021-05-04 17:53:59 -0600

    Input System Madness

---
+++ Assets/Input/InputSystem.cs
@@ -0,0 +1,143 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Input/InputSystem.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @InputSystem : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @InputSystem()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""InputSystem"",
+    ""maps"": [
+        {
+            ""name"": ""Play"",
+            ""id"": ""df32fd3c-83d6-4d72-8cbb-766bd94f9099"",
+            ""actions"": [
+                {
+                    ""name"": ""Pause"",
+                    ""type"": ""Button"",
+                    ""id"": ""d263be59-05b7-45e1-844e-83fe48db0bf9"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""308664de-05e4-439b-86e6-28b15f485d47"",
+                    ""path"": ""<Keyboard>/p"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Pause"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""42812658-39bb-4c22-82a9-ab759d5c2f2c"",
+                    ""path"": ""<Keyboard>/escape"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Pause"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // Play
+        m_Play = asset.FindActionMap("Play", throwIfNotFound: true);
+        m_Play_Pause = m_Play.FindAction("Pause", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Play
+    private readonly InputActionMap m_Play;
+    private IPlayActions m_PlayActionsCallbackInterface;
+    private readonly InputAction m_Play_Pause;
+    public struct PlayActions
+    {
+        private @InputSystem m_Wrapper;
+        public PlayActions(@InputSystem wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Pause => m_Wrapper.m_Play_Pause;
+        public InputActionMap Get() { return m_Wrapper.m_Play; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(PlayActions set) { return set.Get(); }
+        public void SetCallbacks(IPlayActions instance)
+        {
+            if (m_Wrapper.m_PlayActionsCallbackInterface != null)
+            {
+                @Pause.started -= m_Wrapper.m_PlayActionsCallbackInterface.OnPause;
+                @Pause.performed -= m_Wrapper.m_PlayActionsCallbackInterface.OnPause;
+                @Pause.canceled -= m_Wrapper.m_PlayActionsCallbackInterface.OnPause;
+            }
+            m_Wrapper.m_PlayActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Pause.started += instance.OnPause;
+                @Pause.performed += instance.OnPause;
+                @Pause.canceled += instance.OnPause;
+            }
+        }
+    }
+    public PlayActions @Play => new PlayActions(this);
+    public interface IPlayActions
+    {
+        void OnPause(InputAction.CallbackContext context);
+    }
+}
